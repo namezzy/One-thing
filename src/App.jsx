45780069c8129e2660f5119485aa2fd0840245a8
withLevi <zengyin.ssr@gmail.com>
2024-07-29 18:56:31 +0800

    The code is deconstructed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import { ArrowRightCircleIcon } from "@heroicons/react/24/solid";
 
+import CustomForm from "./components/CustomForm";
+
 function App() {
   const [thing, setThing] = useState("");
+  const [isCompleted, setIsCompleted] = useState(true);
 
-  const handleSubmit = (e)=> {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e)
-  }
+    setIsCompleted(false);
+  };
 
   const handleInput = (e) => {
-    console.log(e.target.value)
-  }
+    setThing(e.target.value);
+  };
 
   return (
     <main
@@ -21,39 +24,11 @@ function App() {
     dark:text-slate-200"
     >
       <div className="grid place-items-center gap-8 m-8">
-        <h1
-          className="text-3xl sm:text-6xl font-bold
-        text-center"
-        >
-          What is your “One Thing”?
-        </h1>
-        <form
-          className="flex ring-4 rounded-md ring-slate-200
-         dark:ring-slate-800 focus-within:ring-teal-600
-          focus-within:ring-offset-4 bg-slate-200
-          ring-offset-slate-200 dark:ring-offset-slate-800
-          "
-          onSubmit={handleSubmit}
-        >
-          <input 
-          type="text"
-          className="bg-inherit rounded-md font-sans
-          text-slate-800 py-2 px-6 focus:outline-none
-          text-xl sm:text-2xl placeholder:text-slate-400
-          caret-teal-600 appearance-none w-full"
-          placeholder='Enter One Thing'
-          autoFocus
-          maxLength="64"
-          value={thing}
-          onInput={handleInput}
-          />
-          <button className="bg-inherit rounded-md font-sans
-          text-slate-800 py-2 px-6 focus:outline-none
-          focus:text-teal-600 hover:text-teal-600
-          ">
-            <ArrowRightCircleIcon className="h-12 w-12" />
-          </button>
-        </form>
+        {isCompleted && <CustomForm  
+        thing={thing}
+        handleInput={handleInput}
+        handleSubmit={handleSubmit}
+        />}
       </div>
     </main>
   );
